9349580a65d6b365609e6c0955723fd020e71d76
big-theta <big-theta@MacBook-Pro-admin.local>
2020-08-09 23:20:17 +0300

    custom zsh aliases

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+alias s3="aws s3 --endpoint-url https://s3.selcdn.ru"
+alias ii="idea1 ."
+alias opengitea="open http://192.168.1.41"
+alias openhub="open https://github.com/t0lia"
+
---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -sv "$PWD/.oh-my-zsh/custom/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh"
+
+
