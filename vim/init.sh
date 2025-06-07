9f34fe16587bc65843d20721e744bbeac638b2a1
apozdniakov <apozdniakov@apozdniakovsMBP.fritz.box>
2025-06-08 00:24:18 +0200

    update vimrc

--- vim/init.sh
+++ vim/init.sh
@@ -6,4 +6,13 @@ cd "$(dirname "$0")"
 rm -f ~/.vimrc
 ln -s "$(pwd)/vimrc" ~/.vimrc
 
+# Install vim-plug if not exists
+PLUG_PATH="$HOME/.vim/autoload/plug.vim"
+if [[ ! -f "$PLUG_PATH" ]]; then
+    echo "Installing vim-plug..."
+    curl -fLo "$PLUG_PATH" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 echo "Vim config linked successfully"
+echo "Run ':PlugInstall' in Vim to install plugins"
--- vim/plugins.sh
+++ vim/plugins.sh
@@ -1,23 +1,2 @@
-#!/bin/bash
-
-cd ~/.vim/bundle
-
-git clone https://github.com/kien/ctrlp.vim.git
-
-git clone https://github.com/mattn/emmet-vim.git
-
-git clone https://github.com/Yggdroot/indentLine.git
-
-git clone https://github.com/Raimondi/delimitMate.git
-
-git clone https://github.com/scrooloose/nerdcommenter.git
-
-git clone https://github.com/scrooloose/nerdtree.git
-
-git clone https://github.com/tpope/vim-fugitive.git
-
-git clone https://github.com/airblade/vim-gitgutter.git
-
-git clone https://github.com/pangloss/vim-javascript.git
 
 git clone https://github.com/tpope/vim-surround.git
